00eb37b5bcb0deaae85102b2409140e9b17362bc
Kyevnieves <keyvernievess@gmail.com>
2023-02-17 23:55:29 -0430

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,15 @@
 module.exports = {
   database: {
-    host: "srv-captain--kyevdb-db",
+    host: "srv-captain--kyevdb-db:3306",
     user: "root",
     password: "Fuhrer?*2720",
     database: "database_orders",
-    port: 3306,
     ssl: {
       rejectUnauthorized: false,
     },
   },
 };
-
+console.log("object");
 /* 
   LOCALHOST
    database: {
