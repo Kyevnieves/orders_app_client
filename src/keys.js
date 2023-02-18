125ea2a2c49312e059c8a21081131fd8d522db91
Kyevnieves <keyvernievess@gmail.com>
2023-02-18 13:58:18 -0430

    DotEnvConfiguration

--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,21 @@
+const { config } = require("dotenv");
+config();
+
+const DB_HOST = process.env.DB_HOST || process.env.DB_LOCAL_HOST;
+const DB_USER = process.env.DB_USER || process.env.DB_LOCAL_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD || process.env.DB_LOCAL_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE || process.env.DB_LOCAL_DATABASE;
+
 module.exports = {
   database: {
-    host: "srv-captain--mysqlviejo-db",
-    user: "root",
-    password: "Fuhrer?*2720",
-    database: "database_orders",
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
   },
 };
 /* 
+
   LOCALHOST
    database: {
     host: "localhost",
