880caf329e7ccdcad67ed9c87d5f4739eafcbc35
Kyevnieves <111917540+Kyevnieves@users.noreply.github.com>
2023-02-18 11:50:37 -0400

    routeProductFileIndex

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 app.use(require("./routes"));
 app.use(require("./routes/authentication"));
 app.use(require("./routes/pedidos"));
+app.use(require("./routes/products"));
 app.use("/links", require("./routes/links"));
 
 // PUBLIC
