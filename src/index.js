8035e8cfbd29521d4e9269c88bb6702649f59cbf
Kyevnieves <keyvernievess@gmail.com>
2023-02-17 22:48:32 -0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const passport = require("passport");
 const app = express();
 require("./lib/passport");
 // CONFIGURACIONES
-app.set("port", 4000);
+app.set("port", 80);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
